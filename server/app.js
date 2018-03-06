a1df1069887dcfd8c55a2f0f82149675ff896af3
iamshaunjp <shaunjp@shaun-pelling.com>
2018-03-06 10:32:59 +0000

    lesson 5 code update

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
 
 const app = express();
 
 // bind express with graphql
-app.use('/graphql', expressGraphQL({
+app.use('/graphql', graphqlHTTP({
     // pass in a schema property
-}))
+}));
 
 app.listen(4000, () => {
     console.log('now listening for requests on port 4000');
