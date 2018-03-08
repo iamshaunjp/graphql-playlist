80b0f016e04d3ac589b609724715e57fe2d7f575
iamshaunjp <shaunjp@shaun-pelling.com>
2018-03-08 09:11:05 +0000

    lesson 7 code

---
+++ server/schema/schema.js
@@ -0,0 +1,12 @@
+const graphql = require('graphql');
+
+const { GraphQLObjectType, GraphQLString } = graphql;
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    fields: ( ) => ({
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString }
+    });
+});
