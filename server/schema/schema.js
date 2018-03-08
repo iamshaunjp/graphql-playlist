a92a7ecafe628476b0d30ce92f4c9ebfd3f10632
iamshaunjp <shaunjp@shaun-pelling.com>
2018-03-08 17:04:37 +0000

    lesson 11 code

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,12 @@
 const graphql = require('graphql');
 const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLSchema,
+    GraphQLID
+} = graphql;
 
 // dummy data
 var books = [
@@ -13,7 +18,7 @@ var books = [
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: ( ) => ({
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
         name: { type: GraphQLString },
         genre: { type: GraphQLString }
     })
@@ -24,9 +29,10 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         book: {
             type: BookType,
-            args: { id: { type: GraphQLString } },
+            args: { id: { type: GraphQLID } },
             resolve(parent, args){
                 // code to get data from db / other source
+                console.log(typeof(args.id));
                 return _.find(books, { id: args.id });
             }
         }
