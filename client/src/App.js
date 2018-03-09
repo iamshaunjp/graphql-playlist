699f3e43ce35516604900d558f1cfebd4700e3a8
iamshaunjp <shaunjp@shaun-pelling.com>
2018-03-09 15:13:04 +0000

    lesson 23 code

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+        <div id="main">
+          <h1>Ninja's Reading List</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
