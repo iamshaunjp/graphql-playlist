54216344a03868df04a0c0325bb2c3a964a4c93d
iamshaunjp <shaunjp@shaun-pelling.com>
2018-03-09 16:59:35 +0000

    lesson 27 code

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -12,12 +12,24 @@ const getBooksQuery = gql`
 `;
 
 class BookList extends Component {
+    displayBooks(){
+        var data = this.props.data;
+        if(data.loading){
+            return( <div>Loading books...</div> );
+        } else {
+            return data.books.map(book => {
+                return(
+                    <li key={ book.id }>{ book.name }</li>
+                );
+            })
+        }
+    }
     render(){
         console.log(this.props);
         return(
             <div>
                 <ul id="book-list">
-                    <li>Book name</li>
+                    { this.displayBooks() }
                 </ul>
             </div>
         );
