7a0239ca0a597972bfdfe5b36d5919f6921d3e40
iamshaunjp <shaunjp@shaun-pelling.com>
2018-03-09 11:16:58 +0000

    lesson 16 code

--- server/app.js
+++ server/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// connect to mlab database
+// make sure to replace my db string & creds with your own
+mongoose.connect('mongodb://ninja:test@ds161148.mlab.com:61148/graphql-ninja')
+mongoose.connection.once('open', () => {
+    console.log('conneted to database');
+});
 
 // bind express with graphql
 app.use('/graphql', graphqlHTTP({
