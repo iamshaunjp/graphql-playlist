cc669749bcdd6aa433220dbba42bff2284c7a56b
iamshaunjp <shaunjp@shaun-pelling.com>
2018-03-09 19:06:26 +0000

    lesson 33 code

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { graphql, compose } from 'react-apollo';
-import { getAuthorsQuery, addBookMutation } from '../queries/queries';
+import { getAuthorsQuery, addBookMutation, getBooksQuery } from '../queries/queries';
 
 class AddBook extends Component {
     constructor(props){
@@ -29,7 +29,8 @@ class AddBook extends Component {
                 name: this.state.name,
                 genre: this.state.genre,
                 authorId: this.state.authorId
-            }
+            },
+            refetchQueries: [{ query: getBooksQuery }]
         });
     }
     render(){
