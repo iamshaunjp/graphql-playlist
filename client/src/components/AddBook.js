a404cf713c3f8d7baaf0028511153d6953605e57
iamshaunjp <shaunjp@shaun-pelling.com>
2018-03-09 17:27:07 +0000

    lesson 30 code

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -3,6 +3,14 @@ import { graphql } from 'react-apollo';
 import { getAuthorsQuery } from '../queries/queries';
 
 class AddBook extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            name: '',
+            genre: '',
+            authorId: ''
+        };
+    }
     displayAuthors(){
         var data = this.props.data;
         if(data.loading){
@@ -13,26 +21,29 @@ class AddBook extends Component {
             });
         }
     }
+    submitForm(e){
+        e.preventDefault()
+        console.log(this.state);
+    }
     render(){
         return(
-            <form id="add-book">
+            <form id="add-book" onSubmit={ this.submitForm.bind(this) } >
                 <div className="field">
                     <label>Book name:</label>
-                    <input type="text" />
+                    <input type="text" onChange={ (e) => this.setState({ name: e.target.value }) } />
                 </div>
                 <div className="field">
                     <label>Genre:</label>
-                    <input type="text" />
+                    <input type="text" onChange={ (e) => this.setState({ genre: e.target.value }) } />
                 </div>
                 <div className="field">
                     <label>Author:</label>
-                    <select>
+                    <select onChange={ (e) => this.setState({ authorId: e.target.value }) } >
                         <option>Select author</option>
                         { this.displayAuthors() }
                     </select>
                 </div>
                 <button>+</button>
-
             </form>
         );
     }
