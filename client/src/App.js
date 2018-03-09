8e791ce07427bd445de2ed2a3f81c6dc54a2def2
iamshaunjp <shaunjp@shaun-pelling.com>
2018-03-09 15:31:32 +0000

    lesson 24 code

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+// components
+import BookList from './components/BookList';
+
 class App extends Component {
   render() {
     return (
         <div id="main">
           <h1>Ninja's Reading List</h1>
+          <BookList />
       </div>
     );
   }
---
+++ client/src/components/BookList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class BookList extends Component {
+    render(){
+        return(
+            <div>
+                <ul id="book-list">
+                    <li>Book name</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default BookList;
