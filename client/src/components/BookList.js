a7194c29daa0fc08cd5643a6ac3262f2e777fd82
iamshaunjp <shaunjp@shaun-pelling.com>
2018-03-09 15:47:07 +0000

    lesson 26 code

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
+
+const getBooksQuery = gql`
+    {
+        books {
+            name
+            id
+        }
+    }
+`;
 
 class BookList extends Component {
     render(){
+        console.log(this.props);
         return(
             <div>
                 <ul id="book-list">
@@ -12,4 +24,4 @@ class BookList extends Component {
     }
 }
 
-export default BookList;
+export default graphql(getBooksQuery)(BookList);
--- server/app.js
+++ server/app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+// allow cross-origin requests
+app.use(cors());
 
 // connect to mlab database
 // make sure to replace my db string & creds with your own
