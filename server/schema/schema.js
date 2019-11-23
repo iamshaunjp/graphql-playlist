bf8f8b3ac9422b6959e7a05898206b5be26075fe
Adrian Ross <adriantoddross@gmail.com>
2019-11-23 15:23:07 -0500

    draft RootQuery, args & resolve

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -10,3 +10,16 @@ const BookType = new GraphQLObjectType({
     genre: { type: GraphQLString }
   })
 });
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        // code to get data from database or other source!
+      }
+    }
+  }
+});
