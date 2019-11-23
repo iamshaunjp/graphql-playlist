57c39db4f2eb5c4534f49bb07282ac4459d8a0bc
Adrian Ross <adriantoddross@gmail.com>
2019-11-23 14:49:15 -0500

    Test graphql endpoint

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-
+ 
 const app = express();
+
+app.use('/graphql', graphqlHTTP({
+  
+}));
 
 app.listen(4000, () => {
   console.log('Now listening for requests on port 4000.');
