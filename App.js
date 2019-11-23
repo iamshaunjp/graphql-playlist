30e67d51f7e92aabd6b83a917e8ad5c8679beeed
Adrian Ross <adriantoddross@gmail.com>
2019-11-23 17:01:35 -0500

    reinstall depedencies

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
  
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
-  
+  schema 
 }));
 
 app.listen(4000, () => {
