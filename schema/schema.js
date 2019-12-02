65f9583ca340b80e09c22f8ba348bcff8c5afbe3
Adrian Ross <adriantoddross@gmail.com>
2019-12-01 20:10:46 -0500

    stub resolver with lodash .find

--- schema/schema.js
+++ schema/schema.js
@@ -27,6 +27,7 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString } },
       resolve(parent, args) {
         // code to get data from database or other source!
+        return _.find(books, { id: args.id });
       }
     }
   }
