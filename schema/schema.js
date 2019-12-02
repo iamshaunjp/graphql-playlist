4508609ca126b4bfd86db236ad31a397f72bb422
Adrian Ross <adriantoddross@gmail.com>
2019-12-02 05:53:29 -0500

    add mock data for authors, stub AuthorType

--- schema/schema.js
+++ schema/schema.js
@@ -10,12 +10,27 @@ var books = [
   { name: 'The City & The City', genre: 'Mystery', id: '3' }
 ];
 
+const authors =  [
+  {name: 'Patrick Rothfuss', age: 44, id:"1"},
+  {name: 'Brandon Sanderson', age: 42, id:"2"},
+  {name: 'Terry Pratchett', age: 66, id:"3"},
+]
+
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString }
+  })
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: 'Book',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLString }
   })
 });
 
