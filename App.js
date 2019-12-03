f9fbee796590949049308967f0078beda587ca4d
Adrian Ross <adriantoddross@gmail.com>
2019-12-03 06:20:15 -0500

    require mongoose

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
  
 const app = express();
 
