c780f9e09837d224945037e0e58bfb54aee70356
Prakhar Uniyal <b18128@students.iitmandi.ac.in>
2020-03-28 04:59:26 +0530

    Update schema.js

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,6 @@
 const graphql = require('graphql');
 const Book = require('../models/book');
-const Author = require('../models/Author');
+const Author = require('../models/author');
 const _ = require('lodash');
 
 const {
