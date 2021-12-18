525aef2300ded2fa2394ff0c713b2a47356512ba
Sam Schnaible II <sschnaible83@gmail.com>
2021-12-17 16:24:31 -0800

    setup mongoose connection

--- server/app.js
+++ server/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
 
+mongoose.connect('mongodb+srv://povigard:turock5209%23%23@cluster0.p8p18.mongodb.net/Cluster0?retryWrites=true&w=majority')
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+})
 // bind express with graphql
 app.use('/graphql', graphqlHTTP({
     schema,
