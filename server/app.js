252f7d5dd2bc57312d5b42f17c49adebbce6cd05
erindeji <erinfolami.rasaq@konga.com>
2022-01-03 22:19:26 +0100

    update client app to apollo client 3 and hooks

--- server/app.js
+++ server/app.js
@@ -2,13 +2,12 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
-const cors = require('cors')
 
 const app = express();
 
 // connect to mlab database
 // make sure to replace my db string & creds with your own
-mongoose.connect('mongodb+srv://sa:user12345678@cluster0-eba0o.mongodb.net/graphql-playlist?retryWrites=true&w=majority')
+mongoose.connect('mongodb://ninja:test@ds161148.mlab.com:61148/graphql-ninja')
 mongoose.connection.once('open', () => {
     console.log('conneted to database');
 });
@@ -18,9 +17,6 @@ app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
 }));
-app.use(cors())
-app.options('*', cors());
-app.use(cors({ origin: `http://localhost:3000` }))
 
 app.listen(4000, () => {
     console.log('now listening for requests on port 4000');
